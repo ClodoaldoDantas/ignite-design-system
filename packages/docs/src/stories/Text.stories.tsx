5b5a8aac9e0d84cae5e6efcc1b126d1f27bdbe95
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-11-05 19:36:58 -0300

    feat: Componentes do Design System - Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typograph/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt at quisquam minus sit libero. Ut esse magni fugiat harum consectetur id libero dicta. Odio architecto totam iste, iure ipsa numquam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
