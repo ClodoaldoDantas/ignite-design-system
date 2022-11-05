5958a8a03473c9bd011d326f14dd0b87200fdf45
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-11-05 19:48:41 -0300

    feat: Componentes do Design System - Componente: Heading

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Text, TextProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Typograph/Text',
+  title: 'Typography/Text',
   component: Text,
   args: {
     children:
